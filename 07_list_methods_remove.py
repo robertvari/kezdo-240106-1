4d300794c7844c9c0eee78463ac23c33ad53a1e5
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 16:01:06 +0100

    update

---
+++ 07_list_methods_remove.py
@@ -0,0 +1,8 @@
+# reversed       -4        -3      -2        -1
+my_friends = ["Csaba", "Kriszta", "Tamás", "Géza"]
+
+my_friends.remove("Tamás")
+print(my_friends)
+
+del my_friends[-1]
+print(my_friends)
