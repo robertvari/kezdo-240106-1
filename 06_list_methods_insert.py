1e15c77b699d3d0d2ce29aaf88fd807c863e787c
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 16:22:00 +0100

    update

---
+++ 06_list_methods_insert.py
@@ -0,0 +1,4 @@
+my_friends = ["Kriszta", "Csaba", "Balázs"]
+
+my_friends.insert(0, "Géza")
+print(my_friends)
---
+++ 08_list_methods_replace.py
@@ -0,0 +1,5 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Géza"]
+
+my_friends[0] = "Csilla"
+
+print(my_friends)
