34f7e6c1d94fab11d9013b6ddd6bef6019e2dd66
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 14:43:58 +0100

    update

---
+++ 01_datatypes.py
@@ -0,0 +1,14 @@
+# boolean (bool)
+True, False
+
+# string (str)
+"Hello World"
+'Hello World'
+'It\'s my birthday!'
+"It's my birthday!"
+
+# integer (int)
+1, 2, 3, -1, -2, -3, 100, 200, -100, -200
+
+# float
+3.14, -3.14, 120.12345, -120.12345
