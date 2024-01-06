75173938ef5b7b54b344bdd6a177cbde11e0ce51
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 17:53:20 +0100

    update

--- 16_elif.py
+++ 16_elif.py
@@ -1,13 +1,11 @@
 status = 200
 
+
 if status == 200:
     print("OK")
-
-if status == 300:
+elif status == 300:
     print("Multiple Choices")
-
-if status == 400:
+elif status == 400:
     print("Bad Request")
-
-if status == 500:
+elif status == 500:
     print("Internal Server Error")
