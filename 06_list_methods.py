5c764c1283ff0c12ad1fb07eccc8f0fd0a54bba0
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 15:53:10 +0100

    update

--- 06_list_methods.py
+++ 06_list_methods.py
@@ -2,9 +2,17 @@ lucky_numbers = []
 
 # ask for the number
 number1 = input("Your first number?")
+number2 = input("Your second number?")
+number3 = input("Your third number?")
+number4 = input("Your fourth number?")
+number5 = input("Your fifth number?")
 
 # add number to the list
 lucky_numbers.append(number1)
+lucky_numbers.append(number2)
+lucky_numbers.append(number3)
+lucky_numbers.append(number4)
+lucky_numbers.append(number5)
 
 
 # display list
