4b8ff54efc333368610fa39d516d864042864683
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 17:15:47 +0100

    update

---
+++ 13_dictionary_remove.py
@@ -0,0 +1,6 @@
+phonebook = {
+    #  root key
+    "06201234567": {"name":"Csaba", "address":"Budapest", "email": "csaba@gmail.com"},
+    "06202345678": {"name":"Kriszta", "address":"Debrecen", "email": "kriszta@gmail.com"},
+    "06305427578": {"name":"Tamás", "address":"Pécs", "email": "tamas@gmail.com"},
+}
