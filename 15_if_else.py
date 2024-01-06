b105e2b9fc33b84ead1469846ed6190a3985f15f
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 17:42:40 +0100

    update

---
+++ 15_if_else.py
@@ -0,0 +1,7 @@
+age = 18
+
+#   False
+if age < 18:
+    print("You are under age. You can't drink alcohol.")
+else:
+    print("Now you can drink alcohol. But be careful if you have to drive home.")
