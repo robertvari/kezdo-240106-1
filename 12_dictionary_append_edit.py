18ae4bfe0d079e44e92219b47e10a4c23450d328
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 17:14:50 +0100

    update

--- 12_dictionary_append_edit.py
+++ 12_dictionary_append_edit.py
@@ -1,5 +1,14 @@
 phonebook = {
+    #  root key
     "06201234567": {"name":"Csaba", "address":"Budapest", "email": "csaba@gmail.com"},
     "06202345678": {"name":"Kriszta", "address":"Debrecen", "email": "kriszta@gmail.com"},
     "06305427578": {"name":"Tamás", "address":"Pécs", "email": "tamas@gmail.com"},
 }
+
+# add new data to phonebook
+phonebook["06204256543"] = {"name":"Balázs", "address":"Eger", "email": "balazs@gmail.com"}
+
+# edit/replace data in phonebook
+phonebook["06202345678"]["address"] = "Eger"
+
+pass
