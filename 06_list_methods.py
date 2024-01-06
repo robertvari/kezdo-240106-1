c51d99560ecc6495fccf90a6671b8c226931f5e8
Robert Vari <mail.robertvari@gmail.com>
2024-01-06 15:49:59 +0100

    update

---
+++ 06_list_methods.py
@@ -0,0 +1,11 @@
+lucky_numbers = []
+
+# ask for the number
+number1 = input("Your first number?")
+
+# add number to the list
+lucky_numbers.append(number1)
+
+
+# display list
+print(lucky_numbers)
